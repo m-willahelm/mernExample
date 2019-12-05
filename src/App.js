fc9101b3669659e295975a12637fdf3272019f35
unknown <guimachado007@gmail.com>
2019-12-05 14:34:24 -0300

    ReactCrud completo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Mern from './img/mern.jpeg';
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to={'/'} className="navbar-brand">Guilherme e Núbia</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={'/'} className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/create'} className="nav-link">Create</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/index'} className="nav-link">Index</Link>
+            </li>
+          </ul>
+        </div>
+      </nav> <br/>
+
+        <h2 className={'text-center'}>Aplicação MERN</h2>
+        <img src={Mern} style={{width:650, height:372, marginLeft:'20%'}}/>
+        
+      <Switch>
+          <Route exact path='/create' component={ Create } />
+          <Route path='/edit/:id' component={ Edit } />
+          <Route path='/index' component={ Index } />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
